f207d10cec2eb7a137f44f84e6f3a5552de511be
Parth Adroja <parth2cool2008@gmail.com>
2016-03-27 00:32:49 +0530

    Added Custom TextField View

---
+++ SocialVUE/EmailTF.swift
@@ -0,0 +1,17 @@
+//
+//  EmailTF.swift
+//  SocialVUE
+//
+//  Created by Parth Adroja on 3/27/16.
+//  Copyright © 2016 Parth Adroja. All rights reserved.
+//
+
+import UIKit
+
+class EmailTF: UITextField {
+
+    override func drawRect(rect: CGRect) {
+        EmailTextField.drawCanvas1()
+    }
+
+}
---
+++ SocialVUE/PasswordTF.swift
@@ -0,0 +1,16 @@
+//
+//  PasswordTF.swift
+//  SocialVUE
+//
+//  Created by Parth Adroja on 3/27/16.
+//  Copyright © 2016 Parth Adroja. All rights reserved.
+//
+
+import UIKit
+
+class PasswordTF: UITextField {
+
+    override func drawRect(rect: CGRect) {
+         EmailTextField.drawCanvas2()
+    }
+}
